97158c2418d50d09e5a9f0b8fe2de7ed71480913
lumiknit <aasr4r4@gmail.com>
2022-05-28 19:48:40 +0900

    Add comments and fix bf

--- lpg/lg.lua
+++ lpg/lg.lua
@@ -2,7 +2,36 @@
 
 local lg = {}
 
---- Lua grammar
+-- Create Expr and pass it into `lg.generate`
+
+-- Expr/Stmt List:
+-- hashbang(str) => #!...
+-- comment(Str) => -- ...
+-- raw(Str)
+-- literal(nil | true | false | Num | Str)
+-- op(operator: Str, val: Expr)            # Unary
+-- op(lhs: Expr, operator: Str, rhs: Expr) # Binary
+-- index(val: Expr, idx: Expr) => val[idx]
+-- app(fn: Expr, args: [Expr]) => fn(args)
+-- access(val: Expr, key: Str) => val.key
+-- method(val: Expr, key: Str) => val:key
+-- comma([Expr]) => e1, e2, ...
+-- wrap(Expr) => (e)
+-- table({nil | true | false | Num | Str => Expr})
+-- block([Expr], indent: Maybe Int)
+-- top([Expr])
+-- fn(params: [Str], body: Block, name: Maybe Str)
+-- do_(Block)
+-- set(lhs: Expr, rhs: Expr)
+-- local_(lhs: Expr, rhs:Expr)
+-- if_([cond1, block1, cond2, block2, ..., condn, blockn(, blockelse)])
+-- while_(cond: Expr, block: Block)
+-- for_(v: Str, e1: Expr, e2: Expr, e3: Maybe Expr, body: Block)
+-- for_in(v: Str, r: Expr, body: Block)
+-- return_(v: Expr)
+-- break_()
+
+--- Constant and Helper
 
 local prec_table = {
   ['app'] = 45, ['idx'] = 45, ['acc'] = 45,
@@ -27,31 +56,41 @@ local escapeString = function(s)
   return t:gsub('\\\n', '\\n')
 end
 
-lg.lit = function(v)
+-- Expr/Stmt Constructor
+
+lg.hashbang = function(line)
+  return { '#!', line:gsub('\n.+', ''), sep = 0 }
+end
+
+lg.comment = function(line)
+  return { '--', line:gsub('\n.+', ''), sep = 1 }
+end
+
+lg.raw = function(v)
+  return { v }
+end
+
+lg.literal = function(v)
   if type(v) == 'string' then
-    return { escapeString(v),
-             prec = prec_table['lit'] }
-  else
-    return tostring(v)
+    return { escapeString(v), prec = prec_table.lit }
+  else return { tostring(v) }
   end
 end
 
-lg.unop = function(op, v)
-  return { op, v, prec = prec_table['unary']}
-end
-
-lg.binop = function(lhs, op, rhs)
-  return { lhs, op, rhs,
-           sep = 1, prec = prec_table[op] }
+lg.op = function(a, b, c)
+  if c == nil then -- Unary
+    return { a, b, prec = prec_table.unary }
+  else -- Binary
+    return { a, b, c, sep = 1, prec = prec_table[b] }
+  end
 end
 
 lg.idx = function(v, i)
-  return { v, '[', {i}, ']',
-           prec = prec_table.idx }
+  return { v, '[', {i}, ']', prec = prec_table.idx }
 end
 
 lg.app = function(fn, args)
-  local a = { }
+  local a = {}
   for i, v in ipairs(args) do
     if i > 1 then a[1 + #a] = ', ' end
     a[1 + #a] = v
@@ -59,7 +98,7 @@ lg.app = function(fn, args)
   return { fn, '(', a, ')', prec = prec_table.app }
 end
 
-lg.acc = function(v, k)
+lg.access = function(v, k)
   return { v, '.', k, prec = prec_table.acc }
 end
 
@@ -67,8 +106,21 @@ lg.method = function(v, k)
   return { v, ':', k, prec = prec_table.acc }
 end
 
-lg.tbl = function(tbl)
-  local t = {'{'}
+lg.comma = function(lst)
+  local t = {}
+  for i, v in ipairs(lst) do
+    if i > 1 then t[1 + #t] = ', ' end
+    t[1 + #t] = v
+  end
+  return t
+end
+
+lg.wrap = function(v)
+  return { '(', v, ')' }
+end
+
+lg.table = function(tbl)
+  local t = { '{' }
   local i = 1
   for k, v in pairs(tbl) do
     local z
@@ -86,41 +138,15 @@ lg.tbl = function(tbl)
   return t
 end
 
-lg.block = function(lines)
-  local t = {indent = 1, sep = 2}
-  for i, v in ipairs(lines) do
-    t[i] = v
-  end
+lg.block = function(lines, indent)
+  if indent == nil then indent = 1 end
+  local t = { indent = indent, sep = 2 }
+  for i, v in ipairs(lines) do t[i] = v end
   return t
 end
 
 lg.top = function(lines)
-  local t = {sep = 2}
-  for i, v in ipairs(lines) do
-    t[i] = v
-  end
-  return t
-end
-
-lg.do_ = function(block)
-  return {'do', block, 'end', sep = 2}
-end
-
-lg.comma = function(lst)
-  local t = {}
-  for i, v in ipairs(lst) do
-    if i > 1 then t[1 + #t] = ', ' end
-    t[1 + #t] = v
-  end
-  return t
-end
-
-lg.set = function(lhs, rhs)
-  return {lhs, '=', rhs, sep = 1}
-end
-
-lg.local_ = function(lhs, rhs)
-  return {'local', lhs, '=', rhs, sep = 1}
+  return lg.block(lines, false)
 end
 
 lg.fn = function(params, body, name)
@@ -138,18 +164,27 @@ lg.fn = function(params, body, name)
   return {head, {body}, 'end', sep = 2, prec = prec_table.lit}
 end
 
+lg.do_ = function(block)
+  return { 'do', block, 'end', sep = 2 }
+end
+
+lg.set = function(lhs, rhs)
+  return { lhs, '=', rhs, sep = 1 }
+end
+
+lg.local_ = function(lhs, rhs)
+  return { 'local', lhs, '=', rhs, sep = 1 }
+end
+
 lg.if_ = function(lst)
-  local t = {sep = 2, lines = true}
+  local t = { sep = 2 }
   for i = 1, #lst, 2 do
-    if lst[i + 1] == nil then -- Else branch
+    if lst[i + 1] == nil then -- else branch
+      t[1 + #t] = 'else'
       t[1 + #t] = lst[i]
-    elseif #t == 0 then -- If branch
-      local cond = {"if", lst[i], "then", sep = 1}
-      t[1 + #t] = cond
-      t[1 + #t] = lst[i + 1]
     else
-      local cond = {"elseif", lst[i], "then", sep = 1}
-      t[1 + #t] = cond
+      local l = (#t <= 0) and "if" or "elseif"
+      t[1 + #t] = { l, lst[i], "then", sep = 1 }
       t[1 + #t] = lst[i + 1]
     end
   end
@@ -158,46 +193,43 @@ lg.if_ = function(lst)
 end
 
 lg.while_ = function(cond, body)
-  local c = {"while", cond, "do", sep = 1}
-  return {c, body, 'end', sep = 2}
+  local c = { "while", cond, "do", sep = 1 }
+  return { c, body, 'end', sep = 2 }
 end
 
 lg.for_ = function(v, e1, e2, e3, body)
-  local c = {"for ", v, " = ", e1, ", ", e2 }
+  local c = { "for ", v, " = ", e1, ", ", e2 }
   if e3 ~= nil then
     c[1 + #c] = ', '
     c[1 + #c] = e3
   end
   c[1 + #c] = " do"
-  return {c, body, 'end', sep = 2}
+  return { c, body, 'end', sep = 2 }
 end
 
 lg.for_in = function(v, r, body)
-  local c = {'for', v, 'in', r, 'do', sep = 1}
-  return {c, body, 'end', sep = 2}
+  local c = { 'for', v, 'in', r, 'do', sep = 1 }
+  return { c, body, 'end', sep = 2 }
 end
 
 lg.return_ = function(v)
-  return {'return', v, sep = 1}
+  return { 'return', v, sep = 1 }
 end
 
-lg.break_ = function(v)
-  return {'break'}
+lg.break_ = function()
+  return { 'break' }
 end
 
 --- Flatten
 
 local newBuffer = function()
-  local b = {locs={}, lines={}, buf=nil}
+  local b = { locs = {}, lines = {}, buf = nil }
   b.add = function(self, text)
-    if self.buf == nil then
-      self.buf = text
-    else
-      self.buf = self.buf .. text
-    end
+    self.buf = (self.buf or '') .. text
   end
   b.ln = function(self, indent, loc)
     if self.buf == nil then return end
+    if loc == nil then loc = self.locs[#self.locs] or 0 end
     self.locs[1 + #self.locs] = loc
     self.lines[1 + #self.lines] = ('  '):rep(indent) .. self.buf
     self.buf = nil
@@ -221,8 +253,7 @@ lg.flatten = function(b, idx, src, indent, loc, prec)
   local wrap = false
   if prec ~= nil and src.prec ~= nil then
     local p3 = prec % 3
-    local iz = idx == 1
-    local pcond = (p3 == 1 and not iz) or (p3 == 2 and iz)
+    local pcond = p3 ~= 0 and (p3 == 2) ~= (idx == 1)
     if prec > src.prec or (prec == src.prec and pcond) then
       wrap = true
       if src.sep == 2 then
@@ -236,34 +267,42 @@ lg.flatten = function(b, idx, src, indent, loc, prec)
   -- Traverse
   for i, v in ipairs(src) do
     if i > 1 then
-      if src.sep == 1 then
-        b:add(' ')
-      elseif src.sep == 2 then
-        b:ln(indent, loc)
+      if src.sep == 1 then b:add(' ')
+      elseif src.sep == 2 then b:ln(indent, loc)
       end
       indent = next_indent
     end
-    if type(v) == 'string' then
-      b:add(v)
-    else
-      lg.flatten(b, i, v, indent, loc, src.prec)
+    if type(v) == 'string' then b:add(v)
+    else lg.flatten(b, i, v, indent, loc, src.prec)
     end
   end
   -- Close paren
-  if src.sep == 2 then
-    b:ln(indent, loc)
+  if src.sep == 2 then b:ln(indent, loc) end
+  if wrap then b:add(')') end
+end
+
+lg.reduceLocTable = function(loc)
+  if #loc == 0 then return { 0 } end
+  local t = { 1, loc[1], 1 }
+  for i, v in ipairs(loc) do
+    t[#t] = i
+    if t[#t - 1] ~= v then
+      t[#t + 1] = v
+      t[#t + 1] = i
+    end
   end
-  if wrap then
-    b:add(')')
-  end
+  t[#t] = t[#t] + 1
+  return t
 end
 
 lg.generate = function(src)
   -- Make an indented flatten table
   local b = newBuffer()
-  lg.flatten(b, 0, src, 0, 0, nil)
-  b:ln(0, 0)
-  return table.concat(b.lines, '\n'), b.loc
+  -- Flatten
+  lg.flatten(b, 0, src, 0, nil, nil)
+  -- Then, flush
+  b:ln(0, nil)
+  return table.concat(b.lines, '\n'), lg.reduceLocTable(b.locs)
 end
 
 return lg
--- lpg_examples/bf.lua
+++ lpg_examples/bf.lua
@@ -39,17 +39,18 @@ function parseBF()
   local h = {b}
 
   -- local t = {}
-  table.insert(b, lg.local_('t', lg.tbl({lg.lit(0)})))
+  table.insert(b, lg.local_('t', lg.table({lg.literal(0)})))
   -- local x = 0
-  table.insert(b, lg.local_('x', lg.lit(1)))
+  table.insert(b, lg.local_('x', lg.literal(1)))
 
   local cur_v = lg.idx('t', 'x')
 
-  local zset_cond = lg.binop(cur_v, '==', lg.lit(nil))
-  local zset_ln = lg.set(cur_v, lg.lit(0))
+  local zset_cond = lg.op(cur_v, '==', lg.literal(nil))
+  local zset_ln = lg.set(cur_v, lg.literal(0))
   local zset = lg.if_({zset_cond, lg.block({zset_ln})})
 
   local acc = 0
+  local cmt = false
   while not lp.isEOF() do
     if lp.isStr('+') then
       acc = (acc + 1) % 256
@@ -57,24 +58,25 @@ function parseBF()
       acc = (acc + 255) % 256
     else
       if acc ~= 0 then
-        local a = lg.binop(cur_v, '+', lg.lit(acc))
-        local t = lg.set(cur_v, lg.binop(a, '%', lg.lit(256)))
+        local a = lg.op(cur_v, '+', lg.literal(acc))
+        local t = lg.set(cur_v, lg.op(a, '%', lg.literal(256)))
         table.insert(h[#h], t)
         acc = 0
       end
+      cmt = true
       if lp.isStr('.') then
         local args = {lg.app('string.char', {cur_v})}
         table.insert(h[#h], lg.app('io.write', args))
       elseif lp.isStr(',') then
-        local r = lg.app('io.read', {lg.lit(1)})
+        local r = lg.app('io.read', {lg.literal(1)})
         local y = lg.app('string.byte', {r})
         table.insert(h[#h], lg.set(cur_v, y))
       elseif lp.isStr('>') then
-        local t = lg.set('x', lg.binop('x', '+', lg.lit(1)))
+        local t = lg.set('x', lg.op('x', '+', lg.literal(1)))
         table.insert(h[#h], t)
         table.insert(h[#h], zset)
       elseif lp.isStr('<') then
-        local t = lg.set('x', lg.binop('x', '-', lg.lit(1)))
+        local t = lg.set('x', lg.op('x', '-', lg.literal(1)))
         table.insert(h[#h], t)
         table.insert(h[#h], zset)
       elseif lp.isStr('[') then
@@ -83,9 +85,14 @@ function parseBF()
         if #h == 1 then lp.error('too many closes') end
         z = lg.block(h[#h])
         h[#h] = nil
-        local cond = lg.binop(cur_v, '~=', lg.lit(0))
+        local cond = lg.op(cur_v, '~=', lg.literal(0))
         local w = lg.while_(cond, z)
         table.insert(h[#h], w)
+      else
+        cmt = false
+      end
+      if cmt then
+        table.insert(h[#h], lg.comment(lp.getCurrPosStr()))
       end
     end
     lp.pass()
@@ -101,5 +108,9 @@ end
 src = readFile(in_filename)
 res, ret = lp.parse(parseBF, src, in_filename)
 if res == false then error(ret) end
-local generated = lg.generate(lg.top(ret))
+local generated, loc = lg.generate(lg.top(ret))
+print("Err Loc Table")
+for k, v in pairs(loc) do
+  print(k, v)
+end
 writeFile(out_filename, generated)
